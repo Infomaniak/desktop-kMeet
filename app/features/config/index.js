97b354d2b5cc2fbfe988779cf27e5afceb26f9c9
Goran Urukalo <goran.urukalo@teletrader.com>
2019-06-28 11:59:50 +0200

    fixed problem with custom server and reiew comments

--- app/features/config/index.js
+++ app/features/config/index.js
@@ -28,5 +28,26 @@ export default {
     /**
      * The URL of Terms and Conditions Page.
      */
-    termsAndConditionsURL: 'https://www.infomaniak.com/en/legal/confidentiality-policy'
+    termsAndConditionsURL: 'https://www.infomaniak.com/en/legal/confidentiality-policy',
+
+    /**
+     * Some of default values for application.
+     */
+    defaults: {
+        windowAlwaysOnTop: true
+    },
+
+    /**
+     * All needed configurations for storage in our application
+     * This is including electron-store and redux-persist
+     */
+    storage: {
+        /**
+         * Key where all redux-persist data is stored
+         * Note: FULL_STORE_KEY = redux-persist.KEY_PREFIX + storage.rootKey
+         */
+        rootKey: 'root',
+        settingsKey: 'settings',
+        windowAlwaysOnTopKey: 'windowAlwaysOnTop'
+    }
 };
