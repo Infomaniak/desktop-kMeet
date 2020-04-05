6eed9ecbd4b4387c1a6ab12537333ab01b2e6abe
Anton Buksa <anton.buksa@infomaniak.com>
2020-04-05 21:37:43 +0200

    debug

--- app/features/welcome/components/Welcome.js
+++ app/features/welcome/components/Welcome.js
@@ -102,17 +102,18 @@ class Welcome extends Component<Props, State> {
 
         this.state = { url };
 
-        if (this.props._persistentLang) {
-            i18n.changeLanguage(this.props._persistentLang);
-        } else {
-            (async () => {
-                const detected = await osLocale();
+        // if (this.props._persistentLang) {
+        //     i18n.changeLanguage(this.props._persistentLang);
+        // } else {
+        (async () => {
+            const detected = await osLocale();
 
-                console.log(detected);
+            console.log(detected);
 
-                i18n.changeLanguage(detected.split('-')[0]);
-            })();
-        }
+            i18n.changeLanguage(detected.split('-')[0]);
+        })();
+
+        // }
 
         // Bind event handlers.
         this._onURLChange = this._onURLChange.bind(this);
