ba331b0f4b147c78e30c5983aff02f0ab3be63d1
Léopold Jacquot <leopold.jacquot@infomaniak.com>
2020-09-22 10:58:20 +0200

    replace meet instance url

--- app/features/config/index.js
+++ app/features/config/index.js
@@ -13,7 +13,7 @@ export default {
     /**
      * The default server URL of Jitsi Meet Deployment that will be used.
      */
-    defaultServerURL: 'https://meet.infomaniak.com',
+    defaultServerURL: 'https://kmeet.infomaniak.com',
 
     /**
      * URL to send feedback.
