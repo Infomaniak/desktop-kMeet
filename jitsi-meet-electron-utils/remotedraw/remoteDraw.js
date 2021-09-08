270b4e4fb6ffbaf056f333ced1d37af10307bcab
Thibaut Taelman <thibaut.taelman@infomaniak.com>
2021-09-08 14:17:17 +0200

    fix font faceé

--- jitsi-meet-electron-utils/remotedraw/remoteDraw.js
+++ jitsi-meet-electron-utils/remotedraw/remoteDraw.js
@@ -156,7 +156,7 @@ ipcRenderer.on(SCREEN_SHARE_DRAW_EVENTS_CHANNEL, (event, { data, display }) => {
         nameLabel.style.color = "white";
         nameLabel.style.padding = "5px";
         nameLabel.style.borderRadius = "2px";
-        nameLabel.style.fontFamily = "SuisseIntl";
+        nameLabel.style.fontFamily = "SuisseIntl,Helvetica,verdana,serif";
 
         const txt = document.createTextNode(data.nameLabel);
         nameLabel.appendChild(txt);
