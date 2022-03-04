282649d367c59e65929a64305261911de5fee262
Thibaut Taelman <thibaut.taelman@infomaniak.com>
2022-03-04 10:10:00 +0100

    fix build

--- webpack.main.js
+++ webpack.main.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const SentryCliPlugin = require('@sentry/webpack-plugin');
 
-const plugins = argv.mode === 'development' ? [] : [
+module.exports = (env, argv) => {
+
+    const plugins = argv.mode === 'development' ? [] : [
         new SentryCliPlugin({
             include: '.',
             ignore: [ 'node_modules', 'webpack.config.js' ],
@@ -9,28 +11,30 @@ const plugins = argv.mode === 'development' ? [] : [
         })
     ];
 
-module.exports = {
-    target: 'electron-main',
-    entry: { main: './main.js',
-        preload: './app/preload/preload.js' },
-    output: {
-        path: path.resolve('./build'),
-        filename: '[name].js'
-    },
-    devtool: 'source-map',
-    plugins,
-    node: {
-        __dirname: true
-    },
-    externals: [ {
-        'jitsi-meet-electron-utils': 'require(\'jitsi-meet-electron-utils\')',
-        'electron-debug': 'require(\'electron-debug\')',
-        'electron-reload': 'require(\'electron-reload\')'
-    } ],
-    resolve: {
-        modules: [
-            path.resolve('./node_modules')
-        ]
-    }
+
+    return {
+        target: 'electron-main',
+        entry: { main: './main.js',
+            preload: './app/preload/preload.js' },
+        output: {
+            path: path.resolve('./build'),
+            filename: '[name].js'
+        },
+        devtool: 'source-map',
+        plugins,
+        node: {
+            __dirname: true
+        },
+        externals: [ {
+            'jitsi-meet-electron-utils': 'require(\'jitsi-meet-electron-utils\')',
+            'electron-debug': 'require(\'electron-debug\')',
+            'electron-reload': 'require(\'electron-reload\')'
+        } ],
+        resolve: {
+            modules: [
+                path.resolve('./node_modules')
+            ]
+        }
+    };
 };
 
